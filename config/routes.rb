e4674a2857cb66f49e44e4a2faf34e12ccc73478
Ramzi Hemadou <=>
2023-02-27 00:22:25 +0100

    add controller, views, logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :products
+
+  get "generate_ocr/:id", to: "products#generate_ocr", as: :generate_ocr
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
 end
